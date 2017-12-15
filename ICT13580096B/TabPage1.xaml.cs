6157c4fc30e1ff2837462135c4468063fe47db4e
theerakhanthiyu <easywritell@gmail.com>
2017-12-15 16:40:35 +0700

    zu1

---
+++ ICT13580096B/TabPage1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580096B
+{
+    public partial class TabPage1 : ContentPage
+    {
+        public TabPage1()
+        {
+            InitializeComponent();
+
+
+        }
+    }
+}
---
+++ ICT13580096B/TabPage3.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580096B
+{
+    public partial class TabPage3 : ContentPage
+    {
+        public TabPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
